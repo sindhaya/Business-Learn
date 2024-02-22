8efe0de359b38c9e16b69f72d669d28f342caa96
Mohit Sharma <119623899+sindhaya@users.noreply.github.com>
2024-02-23 02:05:30 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='w-100 bg-light text-center my-5'>
+        <p>@ 2024 BusinessOptima. All Right Reserved | Terms And Conditions Apply !</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer;
